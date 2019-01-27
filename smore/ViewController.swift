3d7dfb81515420bd0cc17febaaf4876c85380ee5
Jing Wei Li <ljw9801055@gmail.com>
2019-01-27 17:10:09 -0500

    Initial Commit

---
+++ smore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smore
+//
+//  Created by Jing Wei Li on 1/27/19.
+//  Copyright © 2019 Jing Wei Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
