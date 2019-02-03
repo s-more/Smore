b40695854ab4899fd5b5660525b14acc0380ea47
Jing Wei Li <ljw9801055@gmail.com>
2019-02-03 14:21:46 -0500

    JL-Added Screens for Welcome, Apple Music Login and Spotify Login

---
+++ smore/Login/SpotifyLoginViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SpotifyLoginViewController.swift
+//  smore
+//
+//  Created by Jing Wei Li on 2/3/19.
+//  Copyright © 2019 Jing Wei Li. All rights reserved.
+//
+
+import UIKit
+
+class SpotifyLoginViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    init() {
+        super.init(nibName: "SpotifyLoginViewController", bundle: Bundle.main)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loginButton.addRoundCorners()
+        usernameTextField.addRoundCorners()
+        passwordTextField.addRoundCorners()
+        // Do any additional setup after loading the view.
+        let colors = [
+            UIColor(red: 0, green: 220/255, blue: 77/255, alpha: 1),
+            UIColor(red: 18/255, green: 138/255, blue: 74/255, alpha: 1)
+        ]
+        loginButton.layer.insertSublayer(CAGradientLayer.gradient(colors: colors,
+                                                                  frame: loginButton.frame), at: 0)
+    }
+    
+    
+    @IBAction func loginSpotifyUser(_ sender: UIButton) {
+        if let username = usernameTextField.text,
+            let password = passwordTextField.text {
+            SpotifyAPI.login(username: username, password: password,
+                             success: {}, error: {})
+        }
+    }
+    
+}
---
+++ smore/Login/WelcomeViewController.swift
@@ -0,0 +1,52 @@
+//
+//  WelcomeViewController.swift
+//  smore
+//
+//  Created by Jing Wei Li on 2/3/19.
+//  Copyright © 2019 Jing Wei Li. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController, UINavigationControllerDelegate {
+    @IBOutlet weak var nextButton: UIButton!
+    
+    init() {
+        super.init(nibName: "WelcomeViewController", bundle: Bundle.main)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.tintColor = UIColor.white
+        navigationController?.navigationBar.barStyle = .black
+        
+        let colors = [
+            UIColor(red: 220/255, green: 148/255, blue: 111/255, alpha: 1),
+            UIColor(red: 132/255, green: 87/255, blue: 64/255, alpha: 1)
+        ]
+        view.layer.insertSublayer(CAGradientLayer.gradient(colors: colors, frame: view.frame), at: 0)
+        nextButton.addRoundCorners()
+        navigationController?.delegate = self
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        navigationController?.pushViewController(AppleMusicAuthViewController(), animated: true)
+    }
+    
+    func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+        let item = UIBarButtonItem(title: " ", style: .plain, target: nil, action: nil)
+        viewController.navigationItem.backBarButtonItem = item
+    }
+}
