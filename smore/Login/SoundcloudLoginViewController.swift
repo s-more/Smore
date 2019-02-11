f0bb22138b47f0fa93d85c8e36b929480502bda0
Sinclair Gurny <sinclairg@hotmail.com>
2019-02-10 20:25:55 -0800

    Added soundcloud xib. Probably missing something

---
+++ smore/Login/SoundcloudLoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SoundcloudLoginViewController.swift
+//  smore
+//
+//  Created by sg on 2/10/19.
+//  Copyright © 2019 Jing Wei Li. All rights reserved.
+//
+
+import UIKit
+
+class SoundcloudLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
