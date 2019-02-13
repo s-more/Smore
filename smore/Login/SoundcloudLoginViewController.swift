1d52003666b9bc9f89630911ae63bb7a74b0b0e0
Sinclair Gurny <sinclairg@hotmail.com>
2019-02-12 21:41:15 -0800

    Added soundcloud xib and connected it to spotify buttons

--- smore/Login/SoundcloudLoginViewController.swift
+++ smore/Login/SoundcloudLoginViewController.swift
@@ -2,29 +2,47 @@
 //  SoundcloudLoginViewController.swift
 //  smore
 //
-//  Created by sg on 2/10/19.
+//  Created by Sinclair Gurny on 2/10/19.
 //  Copyright © 2019 Jing Wei Li. All rights reserved.
 //
 
 import UIKit
 
 class SoundcloudLoginViewController: UIViewController {
-
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var skipButton: UIButton!
+    
+    init() {
+        super.init(nibName: "SoundcloudLoginViewController", bundle: Bundle.main)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        loginButton.addRoundCorners()
+        usernameTextField.addRoundCorners()
+        passwordTextField.addRoundCorners()
+
+        
     }
 
 
-    /*
-    // MARK: - Navigation
+    @IBAction func loginSoundcloudUser(_ sender: UIButton) {
+        /* Do login stuff
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        */
     }
-    */
+    
+    @IBAction func skip(_ sender: UIButton) {
+        
+    }
+    
 
 }
