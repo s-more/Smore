94445f7780864341d3d4a6aa491d2c6565a74431
Jing Wei Li <ljw9801055@gmail.com>
2019-03-16 13:32:15 -0400

    Completed Album Content VC, Added More Requirements to Model Protocols

---
+++ smore/Library/Library Content/NumberedSongTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NumberedSongTableViewCell.swift
+//  smore
+//
+//  Created by Jing Wei Li on 3/15/19.
+//  Copyright © 2019 Jing Wei Li. All rights reserved.
+//
+
+import UIKit
+
+class NumberedSongTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
